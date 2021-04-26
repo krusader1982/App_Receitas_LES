ea4283198fad291d3799417d9fbace9152c729fb
krusader1982 <samuel.xavier@fatec.sp.gov.br>
2021-04-26 10:07:31 -0300

    Update de arquivos 2 Entrega

--- src/__init__.py
+++ src/__init__.py
@@ -3,59 +3,73 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__, template_folder="templates", static_folder="public")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ingredientes.sqlite3'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///receitas.sqlite3'
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
+##### CRIANDO A TABLE RECEITA_INGREDIENTE #####
+receita_ingrediente = db.Table('receita_ingrediente',
+    db.Column('receita_id', db.Integer, db.ForeignKey('receita.id'), primary_key=True),
+    db.Column('ingrediente_id', db.Integer, db.ForeignKey('ingrediente.id'), primary_key=True),
+    db.Column('qtd', db.String())
+)
 
+
+##### CRIANDO A TABLE RECEITA #####
+class Receita(db.Model):
+    __tablename__ = 'receita'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String())
+    modo_preparo = db.Column(db.String())
+    ingredientes = db.relationship('Ingrediente', secondary=receita_ingrediente, lazy='subquery',
+        backref=db.backref('receita', lazy=True))    
+
+    def __init__(self, nome, modo_preparo):
+        self.nome = nome
+        self.modo_preparo = modo_preparo
+        
+
+    def __repr__(self):
+        return '{}'.format(self.nome)
+
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'nome': self.nome,
+            'modo_preparo': self.modo_preparo,
+            }
+
+
+##### CRIANDO A TABLE INGREDIENTE #####
 class Ingrediente(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
-    nome = db.Column(db.String(150), unique=True, nullable=False)
-#    qtd = db.Column(db.Integer)
-    
+    __tablename__ = 'ingrediente'
 
-#    def __init__(self, nome, qtd):
-#        self.nome = nome
-#        self.qtd = qtd
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String())
 
     def __init__(self, nome):
         self.nome = nome
 
+    def __repr__(self):
+        return '{}'.format(self.nome)
 
-#class Receita(db.Model):
-#    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#    nome_receita = db.Column(db.String(50), unique=True, nullable=False)
-#    ingredientes = db.Column(db.String(1000), nullable=False)
-#    modo_preparo = db.column(db.String(5000))
-#    criado_em = db.Column(db.DateTime, server_default=db.func.now())
-#    atualizado_em = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
-#
-#    def __init__(self, nome_receita, ingrediente, modo_preparo):
-#        self.nome = nome
-#        self.ingrediente = ingrediente
-#        self.modo_preparo = modo_preparo
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'nome': self.nome,
+        }
 
-        
-
+ 
 ##### HOME #####
 @app.route("/")
 def home():
     return render_template("home.html")
 
-
 ##### PAGINA DO USUARIO #####
 @app.route("/usuario")
 def user():
     return render_template("Usuario.html")
-
-
-##### ENVIAR RECEITA #####
-@app.route("/enviar_receita")
-def enviar_receita():
-    return render_template("Enviar_receita.html")
-
 
 ##### MINHAS RECEITAS RECEITA #####
 @app.route("/minhas_receitas")
@@ -68,18 +82,14 @@ def ingrediente():
     ingredientes = Ingrediente.query.all()
     return render_template("Ingredientes.html", ingredientes=ingredientes)
 
-
 ##### ADD INGREDIENTE ######
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-#        ingrediente = Ingrediente(request.form['nome'], request.form['qtd'])
         ingrediente = Ingrediente(request.form['nome'])
         db.session.add(ingrediente)
         db.session.commit()
         return redirect(url_for("ingrediente"))
-#    return render_template("add.html")
-
 
 ##### EDITAR INGREDIENTE #####
 @app.route("/edit/<int:id>", methods=["GET", "POST"])
@@ -87,11 +97,9 @@ def edit(id):
     ingrediente = Ingrediente.query.get(id)
     if request.method == "POST":
         ingrediente.nome = request.form['nome']
-#        ingrediente.qtd = request.form["qtd"]
         db.session.commit()
         return redirect(url_for("ingrediente"))
     return render_template("edit.html", ingrediente=ingrediente)
-
 
 ##### DELETAR INGREDIENTE #####
 @app.route("/delete/<int:id>")
@@ -101,66 +109,39 @@ def delete(id):
     db.session.commit()
     return redirect(url_for("ingrediente"))
 
-######################### RECEITA ############################################
-#@app.route("/receita")
-#def receita():
-#    ingredientes = Ingrediente.query.all()
-#    return render_template("Receita.html", ingredientes=ingredientes)
-
-
-######################### RECEITA ############################################
+##### RECEITA #####
 @app.route("/receita")
 def receita():
-    receita = pegar_receita()
-    return render_template("Receita.html", receita=receita)
+    receitas = Receita.query.all()
+    return render_template("receitas.html", receitas=receitas)
 
+##### ADD RECEITA ######
+@app.route("/add_receita", methods=["GET", "POST"])
+def add_receita():
+    if request.method == "POST":
+        receita = Receita(request.form['nome'], request.form['modo_preparo'])
+        db.session.add(receita)
+        db.session.commit()
+        return redirect(url_for("receita"))
 
+##### EDITAR RECEITA #####
+@app.route("/edit_receita/<int:id>", methods=["GET", "POST"])
+def edit_receita(id):
+    receita = Receita.query.get(id)
+    if request.method == "POST":
+        receita.nome = request.form['nome']
+        receita.modo_preparo = request.form['modo_preparo']
+        db.session.commit()
+        return redirect(url_for("receita"))
+    return render_template("edit_receita.html", receita=receita)
 
-######################### RECEITA ############################################
-@app.route("/add_receita", methods=["GET", "POST"] )
-def pegar_receita():
-    return [{
-        'id':1,
-        'name':'Bolo de chocolate',
-        'ingrediente': {
-            'ovo':'3',
-            'oleo': '1 xicara',
-            'chocolate':'1 barra'
-            },'modo_preparo':'bata tudo no liquidicador, coloque em uma assadeira e leve ao forno por 30 minutos',
-            'criado_em': '01/01/2021'
-    }]
-
-
-######################## RECEITA #######################################
-#@app.route("/add", methods=["GET", "POST"])
-#def add():
-#    if request.method == "POST":
-#        ingrediente = Ingrediente(request.form['nome'], request.form['qtd'])
-#        db.session.add(ingrediente)
-#        db.session.commit()
-#        return redirect(url_for("receita"))
-#    return render_template("add.html")
-
-
-######################### RECEITA ######################################
-#@app.route("/edit/<int:id>", methods=["GET", "POST"])
-#def edit(id):
-#    ingrediente = Ingrediente.query.get(id)
-#    if request.method == "POST":
-#        ingrediente.nome = request.form['nome']
-#        ingrediente.qtd = request.form["qtd"]
-#        db.session.commit()
-#        return redirect(url_for("receita"))
-#    return render_template("edit.html", ingrediente=ingrediente)
-
-
-################# RECEITA ################################################
-#@app.route("/delete/<int:id>")
-#def delete(id):
-#    ingrediente = Ingrediente.query.get(id)
-#    db.session.delete(ingrediente)
-#    db.session.commit()
-#    return redirect(url_for("receita"))
+##### DELETAR INGREDIENTE #####
+@app.route("/delete_receita/<int:id>")
+def delete_receita(id):
+    receita = Receita.query.get(id)
+    db.session.delete(receita)
+    db.session.commit()
+    return redirect(url_for("receita"))
 
 
 if __name__ == "__main__":
